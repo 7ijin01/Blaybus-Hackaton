e1a561fdaf55565282460ab74f6861b1c36f0e1e
박기진 <dugudada01@naver.com>
2025-02-10 22:21:06 +0900

    FEAT: 디자이너 entity

---
+++ src/main/java/com/blaybus/reservation/entity/Designer.java
@@ -0,0 +1,28 @@
+package com.blaybus.reservation.entity;
+
+import lombok.*;
+import nonapi.io.github.classgraph.json.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "designers")
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Designer
+{
+    @Id
+    private String id;
+    private String name;
+    private String region;
+    private String address;
+    private Integer price_meet;
+    private Integer price_not_meet;
+    private String profile;
+    private String field;
+    private String introduction;
+    private Integer meet;
+
+}
