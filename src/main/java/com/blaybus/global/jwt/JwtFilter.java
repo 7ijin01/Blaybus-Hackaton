3d0c2a70d43705a11d421067348d5733a322b013
Jinyoung <glitt5384@naver.com>
2025-02-11 22:58:59 +0900

    FEAT: 토큰 콘솔 창에 출력

--- src/main/java/com/blaybus/global/jwt/JwtFilter.java
+++ src/main/java/com/blaybus/global/jwt/JwtFilter.java
@@ -30,6 +30,7 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = resolveToken(request);
+        System.out.println(token);
 
         if (StringUtils.hasText(token) && !jwtUtil.isExpired(token)) {
             Authentication authentication = jwtUtil.getAuthentication(token);
