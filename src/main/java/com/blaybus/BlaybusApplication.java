8d2abc831f93b2524aa30a37ca821828f2b1ed25
박기진 <dugudada01@naver.com>
2025-02-12 19:19:02 +0900

    FEAT: 1시간마다 예약 생성 후 1시간동안 결제완료를 하지 않은 예약 자동 삭제 로직

--- src/main/java/com/blaybus/BlaybusApplication.java
+++ src/main/java/com/blaybus/BlaybusApplication.java
@@ -2,8 +2,10 @@ package com.blaybus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BlaybusApplication {
 
 	public static void main(String[] args) {
