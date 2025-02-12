a9014b60ccc01b0fdc32f89da03381af8a15b538
Jinyoung <glitt5384@naver.com>
2025-02-13 01:52:36 +0900

    DEBUG: redirect-uri 쿠키 미생성

--- src/main/java/com/blaybus/global/oauth2/HttpCookieOAuth2AuthorizationRequestRepository.java
+++ src/main/java/com/blaybus/global/oauth2/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -28,6 +28,9 @@ public class HttpCookieOAuth2AuthorizationRequestRepository
     @Override
     public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest, HttpServletRequest request,
                                          HttpServletResponse response) {
+        System.out.println(authorizationRequest);
+        System.out.println(request);
+        System.out.println(response);
         if (authorizationRequest == null) {
             CookieUtils.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
             CookieUtils.deleteCookie(request, response, REDIRECT_URI_PARAM_COOKIE_NAME);
