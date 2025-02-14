77cd17706da6b69e90c4aa8aa7d29296f0b629f8
Jinyoung <glitt5384@naver.com>
2025-02-14 21:43:10 +0900

    DEBUG: 중복 제거

--- src/main/java/com/blaybus/repository/DesignerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.blaybus.repository;
-
-import com.blaybus.entity.DesignerEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DesignerRepository extends MongoRepository<DesignerEntity, String> {
-}
