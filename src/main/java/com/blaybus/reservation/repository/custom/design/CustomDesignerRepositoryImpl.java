c572893a2ebe3b6469ecf1281c02826d2e68585c
박기진 <dugudada01@naver.com>
2025-02-15 18:03:47 +0900

    FIX: Criteria 중첩 오류 수정

--- src/main/java/com/blaybus/reservation/repository/custom/design/CustomDesignerRepositoryImpl.java
+++ src/main/java/com/blaybus/reservation/repository/custom/design/CustomDesignerRepositoryImpl.java
@@ -20,45 +20,45 @@ public class CustomDesignerRepositoryImpl implements CustomDesignerRepository
     }
 
     @Override
-    public List<Designer> findAllByPriceAndRegion(Integer meet,Integer maxPrice,Integer minPrice, List<String> region) {
-        Query query=new Query();
+    public List<Designer> findAllByPriceAndRegion(Integer meet, Integer maxPrice, Integer minPrice, List<String> region) {
+        Query query = new Query();
 
-        if (meet == 1)
-        {
-            query.addCriteria(Criteria.where("type").is(List.of("대면")));
-            Criteria priceCriteria = new Criteria();
-            if (maxPrice != null) {
-                priceCriteria = priceCriteria.and("price.offline").lte(maxPrice);
+        if (meet == 1) {
+            query.addCriteria(Criteria.where("type").is("대면"));
+            if (maxPrice != null || minPrice != null) {
+                Criteria priceCriteria = Criteria.where("price.offline");
+                if (maxPrice != null) {
+                    priceCriteria = priceCriteria.lte(maxPrice);
+                }
+                if (minPrice != null) {
+                    priceCriteria = priceCriteria.gte(minPrice);
+                }
+                query.addCriteria(priceCriteria);
             }
-            if (minPrice != null) {
-                priceCriteria = priceCriteria.and("price.offline").gte(minPrice);
+        } else if (meet == 0) {
+            query.addCriteria(Criteria.where("type").is("비대면"));
+            if (maxPrice != null || minPrice != null) {
+                Criteria priceCriteria = Criteria.where("price.online");
+                if (maxPrice != null) {
+                    priceCriteria = priceCriteria.lte(maxPrice);
+                }
+                if (minPrice != null) {
+                    priceCriteria = priceCriteria.gte(minPrice);
+                }
+                query.addCriteria(priceCriteria);
             }
-        }
-        else if (meet == 0)
-        {
-            query.addCriteria(Criteria.where("type").is(List.of("비대면")));
-            Criteria priceCriteria = new Criteria();
-            if (maxPrice != null) {
-                priceCriteria = priceCriteria.and("price.online").lte(maxPrice);
-            }
-            if (minPrice != null) {
-                priceCriteria = priceCriteria.and("price.online").gte(minPrice);
-            }
-        }
-        else if (meet == 2)
-        {
-            query.addCriteria(Criteria.where("type").is(List.of("대면","비대면")));
-            Criteria priceCriteria = new Criteria();
-            if (maxPrice != null) {
-                priceCriteria = priceCriteria.orOperator(
-                        Criteria.where("price.online").lte(maxPrice),
-                        Criteria.where("price.offline").lte(maxPrice)
+        } else if (meet == 2) {
+            query.addCriteria(Criteria.where("type").in("대면", "비대면"));
+            if (maxPrice != null || minPrice != null) {
+                Criteria priceCriteria = new Criteria().andOperator(
+                        Criteria.where("price.offline").lte(maxPrice),
+                        Criteria.where("price.online").gte(minPrice)
                 );
+                query.addCriteria(priceCriteria);
             }
         }
 
-        if(region!=null && !region.isEmpty())
-        {
+        if (region != null && !region.isEmpty()) {
             query.addCriteria(Criteria.where("region").in(region));
         }
 
