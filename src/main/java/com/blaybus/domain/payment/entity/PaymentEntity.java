e9e736692f24bb36304b2f5c4c0f30a691b6ce13
Jinyoung <glitt5384@naver.com>
2025-02-17 16:14:07 +0900

    DEBUG: 예약 취소 응답 형식 에러

--- src/main/java/com/blaybus/domain/payment/entity/PaymentEntity.java
+++ src/main/java/com/blaybus/domain/payment/entity/PaymentEntity.java
@@ -1,5 +1,6 @@
 package com.blaybus.domain.payment.entity;
 
+import com.blaybus.domain.payment.dto.AmountDTO;
 import com.blaybus.domain.payment.entity.enums.PaymentMethod;
 import com.blaybus.domain.payment.entity.enums.PaymentStatus;
 import lombok.AllArgsConstructor;
@@ -22,7 +23,7 @@ public class PaymentEntity {
     private String reservationId;  // 예약 ID
     private String userId;
     private String designerId;
-    private double amount;  // 결제 금액
+    private AmountDTO amount;  // 결제 금액
     private PaymentMethod paymentMethod;  // 결제 방식 (카카오페이 / 계좌이체)
     private PaymentStatus status;  // 결제 상태 (PENDING, SUCCESS, FAILED, CANCELLED)
     private LocalDateTime createdAt;  // 결제 요청 시간
