88538cf06cf61dc8fe4cb6e52921da0ee2ee1219
Jinyoung <glitt5384@naver.com>
2025-02-18 19:44:41 +0900

    DEBUG: 주석

--- src/main/java/com/blaybus/domain/payment/controller/PaymentController.java
+++ src/main/java/com/blaybus/domain/payment/controller/PaymentController.java
@@ -1,24 +1,24 @@
-package com.blaybus.domain.payment.controller;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/payment")
-@RequiredArgsConstructor
-@Slf4j
-public class PaymentController {
-
-    @GetMapping("/success")
-    public ResponseEntity<String> paymentSuccess(@RequestParam("pg_token") String pgToken) {
-        log.info("✅ [KakaoPay] 결제 성공: pg_token={}", pgToken);
-
-        // KakaoPay 결제 승인 로직 추가
-        return ResponseEntity.ok("결제 성공! pg_token=" + pgToken);
-    }
-}
+//package com.blaybus.domain.payment.controller;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/payment")
+//@RequiredArgsConstructor
+//@Slf4j
+//public class PaymentController {
+//
+//    @GetMapping("/success")
+//    public ResponseEntity<String> paymentSuccess(@RequestParam("pg_token") String pgToken) {
+//        log.info("✅ [KakaoPay] 결제 성공: pg_token={}", pgToken);
+//
+//        // KakaoPay 결제 승인 로직 추가
+//        return ResponseEntity.ok("결제 성공! pg_token=" + pgToken);
+//    }
+//}
