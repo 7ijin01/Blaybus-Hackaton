4ac15e76aafc52bf54323c0a5cbb81c9bbac7b28
Jinyoung <glitt5384@naver.com>
2025-02-18 16:14:01 +0900

    FIX: 토큰 공백 해결

--- src/main/java/com/blaybus/domain/reservation/service/ReservationService.java
+++ src/main/java/com/blaybus/domain/reservation/service/ReservationService.java
@@ -48,9 +48,9 @@ public class ReservationService
 
     //유저 코드 생성이후 연동할 예정
     public ReservationResponseDto createReservation(String accessToken, ReservationRequestDto requestDto) {
-        String googleId = jwtUtil.getEmail(accessToken);
+        String userId = jwtUtil.getEmail(accessToken.substring(7).trim());
         Reservation reservation=Reservation.buildReservation(requestDto);
-        reservation.setUserId(googleId);
+        reservation.setUserId(userId);
         reservation.setStatus("PENDING");
         reservation.setCreatedAt(Date.from(Instant.now()));
         reservationRepository.save(reservation);
